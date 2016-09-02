f1c0f87caa3957a93d6700c43380597d598a8afb
Zhu Ming <mingzhu.z+github@gmail.com>
2016-09-02 15:20:50 +0800

    Form Validation
    - setup store for react-redux-form

--- imports/modules/tasks/reducers/index.js
+++ imports/modules/tasks/reducers/index.js
@@ -1,5 +1,17 @@
+import {
+  modelReducer,
+  formReducer
+} from 'react-redux-form';
 import Tasks from './main';
 
+const initialTaskState = {
+  text: '',
+  priority: '',
+  checked: false
+};
+
 export default {
+  task: modelReducer('task', initialTaskState),
+  taskForm: formReducer('task', initialTaskState),
   Tasks
 }
