04f5ef2d215fffab88fe3787d21038dbdd7e1b91
Zhu Ming <mingzhu.z+github@gmail.com>
2016-09-04 15:03:10 +0800

    Form Validation
    - add model reducer

--- imports/modules/tasks/components/task-form.js
+++ imports/modules/tasks/components/task-form.js
@@ -12,7 +12,18 @@ import { Field, Form, actions } from 'react-redux-form';
 
 const TaskForm = class extends Component {
   render() {
-    const { addTask } = this.props
+    const { task, textChanged, priorityChanged, addTask } = this.props
+
+    const handleTextChanged = (e) => {
+      console.debug('TaskForm.render(): handleTextChanged(): e.target.value = ', e.target.value);
+      textChanged(e.target.value);
+    }
+
+    const handlePriorityChanged = (e) => {
+      console.debug('handlePriorityChanged(): e.target.value = ', e.target.value);
+      priorityChanged(e.target.value);
+    }
+
     const handleAddTask = (task) => {
       console.debug('TaskForm.handleAddTask(): task = ', task);
       // e.preventDefault();
@@ -38,7 +49,7 @@ const TaskForm = class extends Component {
           <InputGroup>
             <Field model="task.priority">
               <InputGroup.Addon>
-                <select ref="priortyInput">
+                <select ref="priortyInput" onChange={handlePriorityChanged}>
                   <option value="5">High</option>
                   <option value="3" selected>Normal</option>
                   <option value="1">Low</option>
@@ -46,7 +57,7 @@ const TaskForm = class extends Component {
               </InputGroup.Addon>
             </Field>
             <Field model="task.text">
-              <FormControl type="text" ref="taskInput"/>
+              <FormControl componentClass="input" type="text" ref="taskInput" onChange={handleTextChanged} />
             </Field>
             <InputGroup.Button>
               <Button type="submit" bsStyle="info">Add Task</Button>
--- imports/modules/tasks/containers/task-form.js
+++ imports/modules/tasks/containers/task-form.js
@@ -1,11 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { actions } from 'react-redux-form';
+
 import TaskForm from '../components/task-form';
 import { addTask } from  '../actions';
 
-const mapState = () => ({});
+const mapState = (state) => ({task: state.task});
 const mapDispatch = (dispatch, getState) => {
   return {
+  	textChanged: (text) => actions.change('task.text', text),
+  	priorityChanged: (priority) => actions.change('task.priority', priority),
     addTask: (taskId) => dispatch(addTask(taskId))
   }
 };
